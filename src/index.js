c70f1fca2187317be72949a0dca3c969a8d3bd7b
Luiz Casimiro <luizcasimi@gmail.com>
2022-08-26 17:28:14 -0300

    text updates

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
 
 // The Provider component uses React context under the hood, and will provide the store to every component in your application.
 
-// Once react-redux is installed, and the Provider is set up, we can read values from the store (with useSelector) and dispatch actions to the store (withUseDispatch).
+// Once react-redux is installed, and the Provider is set up, we can read values from the store (with useSelector) and dispatch actions to the store (with useDispatch).
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
